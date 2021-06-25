fb9e6e61d58462cf50fe226c8f3358f8bad06149
ad72905 <ashish.dalal.bitspilani@gmail.com>
2021-06-26 01:46:42 +0530

    learnings and scripts

---
+++ Data_Engineer/Intro_to_Python_Programming/scripts/password_generator.py
@@ -0,0 +1,32 @@
+# TODO: First import the `random` module
+import random
+
+# We begin with an empty `word_list`
+word_file = "words.txt"
+word_list = []
+
+# We fill up the word_list from the `words.txt` file
+with open(word_file,'r') as words:
+	for line in words:
+		# remove white space and make everything lowercase
+		word = line.strip().lower()
+		# don't include words that are too long or too short
+		if 3 < len(word) < 8:
+			word_list.append(word)
+
+# TODO: Add your function generate_password below
+# It should return a string consisting of three random words 
+# concatenated together without spaces
+def generate_password():
+    password_contents = random.sample(word_list,3)
+    return ''.join(password_contents)
+
+def generate_alternate_password():
+    return random.choice(word_list) + random.choice(word_list)\
+            + random.choice(word_list)
+
+# Now we test the function
+print('Password\n')
+print(generate_password() + '\n')
+print('Alternate Password\n')
+print(generate_alternate_password())
